a06bb4842724b6dbbc4032e749521d3d07f0e389
PiiYukana01 <104289571+PiiYukana01@users.noreply.github.com>
2023-01-18 20:36:30 +0700

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -1,66 +1,66 @@
 let allMusic = [{
         name: "Trời Giấu Trời Mang Đi",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "music-1",
         src: "music-1"
     },
     {
         name: "Giấc Mơ Rất Thơ",
-        artist: "MLưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "music-2",
         src: "music-2"
     },
     {
         name: "Tình Thật Tình Mơ",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "music-3",
         src: "music-3"
     },
     {
         name: "Đắp Mộ Magaming",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "2",
         src: "2"
     },
     {
         name: "Thương Cho Tấm Thân",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "3",
         src: "3"
     },
     {
         name: "Thà Yêu Lấy Một Người Bình Thường",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "4",
         src: "4"
     },
     {
         name: "Ngàn Câu Hứa",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "5",
         src: "5"
     },
     {
         name: "Nắm Tay Nhau",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "6",
         src: "6"
     },
     {
         name: "Sợ Ngày Mai Em Đi Mất",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "7",
         src: "7"
     },
     {
         name: "Trót Trao Duyên",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "8",
         src: "8"
     },
     {
         name: "Ngày Thơ ẤU",
-        artist: "Lưu Xuân Nghiệp",
+        artist: "Nguyễn Hoàng Dũng",
         img: "9",
         src: "9"
     },
